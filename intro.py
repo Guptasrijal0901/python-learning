c46783f2bcec51f44264e2adc58daf61ecce1d6d
Srijal Gupta <guptasrijal0901@gmail.com>
2024-08-10 00:13:55 +0530

    lol

--- intro.py
+++ intro.py
@@ -1,12 +1,25 @@
-name = "Srijal"
-age = 23
-print("Hello world")
-print( name, age)
+# to print data
+# name = "Srijal"
+# age = 23
+# print("Hello world")
+# print( name, age)
 
-name = "Shreya"
-age= 22
-print(name, age)
+# name = "Shreya"
+# age= 22
+# print(name, age)
 
-is_adult = False
-print(is_adult)
+# is_adult = False
+# print(is_adult)
 
+# to take input 
+# name = input("What is you name? ")
+# print("Hello "+ name)
+
+# type conversion 
+# old_age = input("You old age? ")
+
+# new_age = int(old_age) + 2 
+# print(new_age)
+
+number = 9
+print(float(number))
---
+++ sum.py
@@ -0,0 +1,5 @@
+first = input("Enter 1st number: ")
+second = input("Enter 2nd number: ")
+
+sum = first + second
+print("Sum is: "+sum)
